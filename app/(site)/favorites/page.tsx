7bf352858b6efe53f53b6fd16e0c01c697c0fd69
arabeen100 <125078824+arabeen100@users.noreply.github.com>
2026-01-11 19:37:53 +0200

    Update favorites page item count display style

--- app/(site)/favorites/page.tsx
+++ app/(site)/favorites/page.tsx
@@ -12,7 +12,7 @@ const Favorites = () => {
     <main className={`pt-38  mr-4 ml-4 xl:mx-auto xl:w-300 flex flex-col gap-6 `}>
       <div className="flex justify-between">
         <p className="text-white text-4xl font-extrabold">Your<span className="text-icon1  ml-2">Favorites</span></p>
-        <div className="text-secondary text-center p-1.5 text-sm rounded-lg border border-secondary/30 bg-gray-800/90 w-fit">{savedRecipes?.length} items saved</div>
+        <div className="text-secondary text-center p-1 text-xs rounded-full border border-secondary/30 bg-gray-800/90 w-fit">{savedRecipes?.length} items saved</div>
       </div>
       <div className="grid  md:grid-cols-2 min-[1060px]:grid-cols-4 gap-5 w-full auto-rows-fr">
       <RecipeCard recipes={savedRecipes}/>
